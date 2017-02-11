5430d5031fb4da188e1a4162649b5fb9c980186f
Tobin Yehle <tobinyehle@gmail.com>
2017-02-10 20:51:11 -0700

    update base difinitions and clean up the parser a bit

--- src/HL/Parser.hs
+++ src/HL/Parser.hs
@@ -3,23 +3,26 @@ module HL.Parser (parseProgram, parseModule) where
 import HL.AST
 import Text.Parsec
 import Control.Monad (void)
-import qualified Data.Bifunctor as Bifunctor (first)
+import Data.Bifunctor (first)
 
 parseProgram :: String -> String -> Either String Program
-parseProgram name = Bifunctor.first show . lexParse programP name
+parseProgram = lexParse programP
 
 parseModule :: String -> String -> Either String [Definition]
-parseModule name = Bifunctor.first show . lexParse moduleP name
+parseModule = lexParse moduleP
 
 type Parser a = Parsec String () a
 
-lexParse :: Parser a -> String -> String -> Either ParseError a
-lexParse p name input = parse commentP name input >>= parse p name
+lexParse :: Parser a -> String -> String -> Either String a
+lexParse p name input = first show $ parse commentP name input >>= parse p name
 
 commentP :: Parser String
-commentP = concat <$> sepEndBy (many (noneOf ";")) (char ';' *> many (noneOf "\n\r"))
+commentP = concat <$> sepEndBy (many (noneOf ";"))
+                               (char ';' *> many (noneOf "\n\r"))
 
 -- <prg> ::= <def> ... <exp>
+--
+-- <mod> ::= <def> ...
 --
 -- <def> ::= (define <var> <exp>)
 --        |  (define (<var> <arg> ...) <exp>)
@@ -28,13 +31,15 @@ commentP = concat <$> sepEndBy (many (noneOf ";")) (char ';' *> many (noneOf "\n
 --
 --        |  <nat>
 --
---        |  (λ (<arg> ...) <exp>)
+--        |  (<lam> (<arg> ...) <exp>)
 --        |  (let ((<var> <exp>) ...) <exp>)
 --        |  (letrec (<var> <exp>) <exp>)
 --
 --        |  (<exp> <exp> ...)
 --
 -- <arg> ::= _ | <var>
+--
+-- <lam> ::= λ | lambda
 
 moduleP :: Parser [Definition]
 moduleP = spaces *> sepEndBy definitionP whitespace <* eof
@@ -106,7 +111,7 @@ appP = inParens $ do
 
 -- must not include whitespace or ")}]"
 identifierP :: Parser String
-identifierP = (:) <$> (lower <|> symbols) <*> many (alphaNum <|> symbols)
+identifierP = (:) <$> (lower <|> symbols) <*> many (alphaNum <|> symbols <|> char '_')
   where
     symbols = oneOf "!@#$%^&*+-=<>?/~"
 
